2485c362c6583ede4ebaeddee4728db6c30898c7
Samuel Yallop <sam@metsys-d>
2012-11-10 01:39:05 +0000

    Removes previous implementation attempt. Adds interfaces under subdirectory

--- CaptureManager/AbstractCapturer.h
+++
@@ -1,9 +0,0 @@
-/*
- * The AbstractCapturer class provides an interface that a capturer object should implement
- */
-
-class AbstractCapturer {
-    public:
-        virtual void tick(int maxPackets) = 0;
-        virtual string getParsedPackets() = 0;
-};
