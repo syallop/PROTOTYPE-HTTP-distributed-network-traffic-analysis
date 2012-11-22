4cc8bd4bcdd30c2a81e35891086cc593640b6721
say01u <say01u@ad6cae3c-13a9-11e2-a892-2505c92e46a8>
2012-11-22 00:56:58 +0000

    Removes previous implementation attempt. Adds interfaces under subdirectory
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-rmm/prototypes@26 ad6cae3c-13a9-11e2-a892-2505c92e46a8

---
+++ CaptureManager/Interfaces/AbstractCapturer.h
@@ -0,0 +1,18 @@
+#ifndef ABSTRACTCAPTURER_H
+#define ABSTRACTCAPTURER_H
+
+/*
+ * The AbstractCapturer class provides an interface that capturers should implement.
+ * A Capturer is something that parses a source of pcap data producing a string of output.
+ *
+ * Note: The source of the data is left to be defined by the implementor
+ */
+#include <string>
+
+class AbstractCapturer {
+    public:
+        virtual void tick(int maxPackets) = 0;     //Ask a capturer to update its internal state
+        virtual std::string getParsedPackets() = 0;//Ask a capturer to return parsed packets
+};
+
+#endif
